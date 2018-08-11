4bb8d3085a74c8231ee7ef50a39696abcd08b501
Wolfgang Fahl <wf@bitplan.com>
2018-08-11 14:08:25 +0200

    adds functions to be implemented

--- simplegraph-caldav/src/main/java/com/bitplan/simplegraph/caldav/CaldavSystem.java
+++ simplegraph-caldav/src/main/java/com/bitplan/simplegraph/caldav/CaldavSystem.java
@@ -1,5 +1,35 @@
 package com.bitplan.simplegraph.caldav;
 
-public class CaldavSystem {
+import com.bitplan.simplegraph.core.SimpleNode;
+import com.bitplan.simplegraph.core.SimpleSystem;
+import com.bitplan.simplegraph.impl.SimpleSystemImpl;
+
+public class CaldavSystem extends SimpleSystemImpl{
+
+	/**
+	 * initialize me
+	*/
+	public	CaldavSystem() {
+		super.setName("CaldavSystem");
+		super.setVersion("0.0.1");
+	}	
+	
+	@Override
+	public SimpleSystem connect(String... connectionParams) throws Exception {
+		// TODO Auto-generated method stub
+	    return this;
+	}
+
+	@Override
+	public SimpleNode moveTo(String nodeQuery, String... keys) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Class<? extends SimpleNode> getNodeClass() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
--- simplegraph-carddav/src/main/java/com/bitplan/simplegraph/carddav/CarddavSystem.java
+++ simplegraph-carddav/src/main/java/com/bitplan/simplegraph/carddav/CarddavSystem.java
@@ -1,5 +1,35 @@
 package com.bitplan.simplegraph.carddav;
 
-public class CarddavSystem {
+import com.bitplan.simplegraph.core.SimpleNode;
+import com.bitplan.simplegraph.core.SimpleSystem;
+import com.bitplan.simplegraph.impl.SimpleSystemImpl;
+
+public class CarddavSystem extends SimpleSystemImpl{
+
+	/**
+	 * initialize me
+	*/
+	public	CarddavSystem() {
+		super.setName("CarddavSystem");
+		super.setVersion("0.0.1");
+	}
+	
+	@Override
+	public SimpleSystem connect(String... connectionParams) throws Exception {
+		// TODO Auto-generated method stub
+	    return this;
+	}
+
+	@Override
+	public SimpleNode moveTo(String nodeQuery, String... keys) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Class<? extends SimpleNode> getNodeClass() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
