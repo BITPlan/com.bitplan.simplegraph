9f40fcfc71fb79a897acf710fc9962a6c76dcd3f
Wolfgang Fahl <wf@bitplan.com>
2019-02-13 16:18:04 +0100

    tries fixing deploy issue see https://stackoverflow.com/questions/40964500/maven-jar-plugin-3-0-2-error-you-have-to-use-a-classifier-to-attach-supplementa/40964626

---
+++ simplegraph-bundle/src/main/java/com/bitplan/simplegraph/bundle/SimpleGraphBundle.java
@@ -0,0 +1,32 @@
+/**
+ * Copyright (c) 2018 BITPlan GmbH
+ *
+ * http://www.bitplan.com
+ *
+ * This file is part of the Opensource project at:
+ * https://github.com/BITPlan/com.bitplan.simplegraph
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.bitplan.simplegraph.bundle;
+
+/**
+ * i am a totally useless class to work around mvn's bad behavior
+ * see
+ * http://maven.40175.n5.nabble.com/quot-mvn-clean-verify-deploy-quot-causes-jar-plugin-to-execute-twice-td5877166.html
+ * @author wf
+ *
+ */
+public class SimpleGraphBundle {
+
+}
